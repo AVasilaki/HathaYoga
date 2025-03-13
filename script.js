cb9aebea6cb148cbd15d83a29b53d3bf5180072d
AVasilaki <poseydon463@gmail.com>
2025-03-13 22:53:08 +0200

    script

--- script.js
+++ script.js
@@ -11,13 +11,36 @@ function handleSubmit(evt) {
   const elements = form.elements;
   const asanaName = elements[0].children[1].textContent;
   const asanaImg = elements[0].children[3].src;
-  console.log(asanaImg);
+
   const timeAsana = Number(elements.time.value);
   const asanaRest = Number(elements.timeBreake.value);
+
+  const id = form.id;
+
+  const btnDel = elements.delete;
+  btnDel.addEventListener("click", handleDelete);
+
+  function handleDelete(evt) {
+    console.log(id);
+    // console.log(asanas.indexOf(asanas.id === id));
+    const i = asanas.findIndex((el) => el.id === id);
+    console.log(i);
+    asanas.splice(i, 1);
+    asanas.map((el) => console.log(el));
+    added.innerHTML = "";
+    const m = asanas
+      .map((el) => {
+        return `<h3>${el.asanaName}</h3>`;
+      })
+      .join("");
+    added.insertAdjacentHTML("beforeend", m);
+    // console.log(evt.target.form);
+  }
   // const fields = Array.from(form.children);
   // const deskr = fields[0];
   // console.log(form[0].children[2].textContent);
   const obj = {
+    id,
     asanaName,
     asanaImg,
     timeAsana,
@@ -26,10 +49,11 @@ function handleSubmit(evt) {
 
   asanas.push(obj);
   added.style = "display:block";
-  const murkup = `<h2>added asanas</h2><h3>${asanaName}</h3>`;
+
+  const murkup = `<h3>${asanaName}</h3>`;
   added.insertAdjacentHTML("beforeend", murkup);
 }
-console.log(asanas);
+
 function startTraining() {
   document.getElementById("myAudio").play();
 }
