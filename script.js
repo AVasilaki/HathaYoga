15549f0e45e615ce7bd8da51215953ed08c772f9
AVasilaki <poseydon463@gmail.com>
2025-03-14 12:17:26 +0200

    script

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let asanas = [];
+let totalCount = 0;
+
 const list = document.querySelector("ol");
 const body = document.querySelector("body");
 const added = document.querySelector(".added");
-console.log(added);
+// console.log(added);
 
 list.addEventListener("submit", handleSubmit);
 function handleSubmit(evt) {
@@ -17,28 +19,23 @@ function handleSubmit(evt) {
 
   const id = form.id;
 
-  const btnDel = elements.delete;
-  btnDel.addEventListener("click", handleDelete);
+  // const btnDel = elements.delete;
+  // btnDel.addEventListener("click", handleDelete);
 
-  function handleDelete(evt) {
-    console.log(id);
-    // console.log(asanas.indexOf(asanas.id === id));
-    const i = asanas.findIndex((el) => el.id === id);
-    console.log(i);
-    asanas.splice(i, 1);
-    asanas.map((el) => console.log(el));
-    added.innerHTML = "";
-    const m = asanas
-      .map((el) => {
-        return `<h3>${el.asanaName}</h3>`;
-      })
-      .join("");
-    added.insertAdjacentHTML("beforeend", m);
-    // console.log(evt.target.form);
-  }
-  // const fields = Array.from(form.children);
-  // const deskr = fields[0];
-  // console.log(form[0].children[2].textContent);
+  // function handleDelete(evt) {
+  //   const i = asanas.findIndex((el) => el.id === id);
+
+  //   asanas.splice(i, 1);
+
+  //   added.innerHTML = "";
+  //   const m = asanas
+  //     .map((el) => {
+  //       return `<h3>${el.asanaName}</h3>`;
+  //     })
+  //     .join("");
+  //   added.insertAdjacentHTML("beforeend", m);
+  // }
+
   const obj = {
     id,
     asanaName,
@@ -50,8 +47,20 @@ function handleSubmit(evt) {
   asanas.push(obj);
   added.style = "display:block";
 
-  const murkup = `<h3>${asanaName}</h3>`;
-  added.insertAdjacentHTML("beforeend", murkup);
+  // const murkup = `<h3>${asanaName}</h3>`;
+  // added.insertAdjacentHTML("beforeend", murkup);
+  added.innerHTML = "";
+
+  const m = asanas
+    .map((el) => {
+      return `<h3>${el.asanaName}</h3><button type='button' onclick=''>del</button>`;
+    })
+    .join("");
+  added.insertAdjacentHTML("beforeend", m);
+  totalCount = totalCount + timeAsana + asanaRest;
+  const totalPracticeTime = `<p>total time ${totalCount}</p> `;
+  added.insertAdjacentHTML("beforeend", totalPracticeTime);
+  console.log(asanas);
 }
 
 function startTraining() {
