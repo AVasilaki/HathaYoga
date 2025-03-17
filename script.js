4efd395bfd0ce15e685ed396cebe14f9fe2ed58d
AVasilaki <poseydon463@gmail.com>
2025-03-17 13:48:41 +0200

    exelent delete asanas

--- script.js
+++ script.js
@@ -1,45 +1,26 @@
 let asanas = [];
-let totalCount = 0;
-let count = 0;
+let totalTime = 0;
+let numberOfAsanas = 0;
 
 const list = document.querySelector("ol");
 const body = document.querySelector("body");
 const added = document.querySelector(".added");
-// console.log(added);
 
 list.addEventListener("submit", handleSubmit);
+
 function handleSubmit(evt) {
   evt.preventDefault();
   const form = evt.target;
   const elements = form.elements;
   const asanaName = elements[0].children[1].textContent;
   const asanaImg = elements[0].children[3].src;
-
   const timeAsana = Number(elements.time.value);
   const asanaRest = Number(elements.timeBreake.value);
-
   const id = form.id;
-
-  // const btnDel = elements.delete;
-  // btnDel.addEventListener("click", handleDelete);
-
-  // function handleDelete(evt) {
-  //   const i = asanas.findIndex((el) => el.id === id);
-
-  //   asanas.splice(i, 1);
-
-  //   added.innerHTML = "";
-  //   const m = asanas
-  //     .map((el) => {
-  //       return `<h3>${el.asanaName}</h3>`;
-  //     })
-  //     .join("");
-  //   added.insertAdjacentHTML("beforeend", m);
-  // }
 
   const obj = {
     id,
-    index: count,
+    index: Date.now(),
     asanaName,
     asanaImg,
     timeAsana,
@@ -47,47 +28,54 @@ function handleSubmit(evt) {
   };
 
   asanas.push(obj);
-  count += 1;
-  console.log(count);
-  added.style = "display:block";
-  murkup();
-  // const murkup = `<h3>${asanaName}</h3>`;
-  // added.insertAdjacentHTML("beforeend", murkup);
-  // added.innerHTML = "";
+  numberOfAsanas += 1;
 
-  // const m = asanas
-  //   .map((el) => {
-  //     const i = el.index;
-  //     return `<h3>${el.asanaName}</h3>
-  //     <button type='button' id='${i}'  onclick='deleteAsana(${i})'>del</button>`;
-  //   })
-  //   .join("");
-  // added.insertAdjacentHTML("beforeend", m);
-  totalCount = totalCount + timeAsana + asanaRest;
-  const totalPracticeTime = `<p>total time ${totalCount}</p> `;
-  added.insertAdjacentHTML("beforeend", totalPracticeTime);
-  console.log(asanas);
+  added.style = "display:block";
+
+  const timeOneAsana = timeAsana + asanaRest;
+  totalTime += timeOneAsana;
+
+  const totalPracticeTime = `<p>total time ${totalTime}</p> `;
+
+  murkup(totalPracticeTime);
+}
+
+function deleteAsana(i) {
+  const deletingAsana = asanas.find((el) => el.index === i);
+
+  const timeofAsana = deletingAsana.timeAsana;
+  const restTime = deletingAsana.asanaRest;
+  totalTimeAsana = timeofAsana + restTime;
+
+  totalTime -= totalTimeAsana;
+  const totalPracticeTime = `<p>total time ${totalTime}</p> `;
+
+  const n = asanas.findIndex((el) => el.index === i);
+
+  asanas.splice(n, 1);
+
+  numberOfAsanas -= 1;
+  murkup(totalPracticeTime);
+}
+
+function murkup(totalPracticeTime) {
+  added.innerHTML = "";
+  if (!asanas.length) {
+    added.style = "display:none";
+  }
+  const m = asanas
+    .map((el) => {
+      const i = el.index;
+      return `<div class='addeAasanaContainer'>
+      <h3> ${el.asanaName}</h3>
+      <button type='button' id='${i}'  onclick='deleteAsana(${i})'>del</button> 
+      </div>`;
+    })
+    .join("");
+  const murkup = m + totalPracticeTime;
+  added.insertAdjacentHTML("beforeend", murkup);
 }
 
 function startTraining() {
   document.getElementById("myAudio").play();
-}
-function deleteAsana(i) {
-  console.log("i", i);
-  asanas.splice(i, 1);
-  count -= 1;
-  murkup();
-  // console.log(asanas);
-}
-function murkup() {
-  added.innerHTML = "";
-
-  const m = asanas
-    .map((el) => {
-      const i = el.index;
-      return `<h3>${el.asanaName}</h3>
-      <button type='button' id='${i}'  onclick='deleteAsana(${i})'>del</button>`;
-    })
-    .join("");
-  added.insertAdjacentHTML("beforeend", m);
 }
